7205aa6d0da486c0104328f636b2e178f7b1e3dd
crakesh20 <94193705+crakesh20@users.noreply.github.com>
2022-11-28 17:53:23 -0500

    Update DoubleLL.java

--- Project_4/src/DoubleLL.java
+++ Project_4/src/DoubleLL.java
@@ -1,3 +1,28 @@
+
 public class DoubleLL {
+
+    private Node curr;
+    private Node next;
+    private Node prev;
+    int size;
+
+    public DoubleLL(Node first) {
+        curr = first;
+        next = null;
+        prev = null;
+        size = 0;
+    }
+
+    public void insert(Node n) {
+        
+    }
+
+    public void remove(Node n) {
+        
+    }
+
+    public void printAll() {
+        
+    }
 
 }
