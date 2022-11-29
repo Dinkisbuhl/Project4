f1ccb06fd51df9cccde4ce197cb1b29b48a1e64a
Dinkisbuhl <hilgenkj20@vt.edu>
2022-11-29 01:18:11 -0500

    Insert and Search

--- Project_4/src/HashTable.java
+++ Project_4/src/HashTable.java
@@ -2,16 +2,45 @@
 public class HashTable {
 
     private int tableSize;
-    private HashRecord[] theTable;
+    private HashRecord[] hT;
+    private String EMPTYKEY = "";
 
     public HashTable(int s) {
         tableSize = s;
-        theTable = new HashRecord[tableSize];
+        hT = new HashRecord[tableSize];
     }
 
 
-    public void insert() {
+    // Insert e into hash table HT
+    public void hashInsert(String k, String e) {
+        int home = sFoldHash(k); // Home position for e
+        int pos = home; // Init probe sequence
+        for (int i = 1; EMPTYKEY != (hT[pos]).getKey(); i++) {
+            if (k == hT[pos].getKey()) {
+                System.out.println("Duplicates not allowed");
+                return;
+            }
+            //pos = (home + p(k, i)) % tableSize; // probe
+        }
+        hT[pos].setValue(e);
+    }
 
+
+    // Search for the record with Key K
+    public boolean hashSearch(String K, String e) {
+        int home = sFoldHash(K); // Home position for K
+        int pos = home; // Initial position is the home slot
+        for (int i = 1; (K != (hT[pos]).getKey()) && (EMPTYKEY != (hT[pos])
+            .getKey()); i++) {
+            //pos = (home + p(K, i)) % tableSize; // Next on probe sequence
+        }
+        if (K == (hT[pos]).getKey()) { // Found it
+            e = hT[pos].getValue();
+            return true;
+        }
+        else {
+            return false;
+        } // K not in hash table
     }
 
 
@@ -49,6 +78,20 @@ public class HashTable {
 
         public HashRecord(String k, String v) {
             key = k;
+            value = v;
+        }
+
+
+        private String getKey() {
+            return key;
+        }
+
+
+        private String getValue() {
+            return value;
+        }
+        
+        private void setValue(String v) {
             value = v;
         }
     }
---
+++ Project_4/src/HashTableTest.java
@@ -0,0 +1,12 @@
+import student.TestCase;
+public class HashTableTest extends TestCase {
+    
+    HashTable hT;
+    /**
+     * Sets up the Node for the
+     * test methods
+     */
+    public void setUp() {
+        hT = new HashTable(10);
+    }
+}
